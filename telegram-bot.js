5b6eac5022ec3e57c074070e6b82f5d23d82a716
s79912 <s79912@beuth-hochschule.de>
2021-02-07 02:24:01 +0100

    fix error message from response

--- telegram-bot.js
+++ telegram-bot.js
@@ -87,8 +87,8 @@ function respond(chatId, botResponse){
         bot.sendMessage(chatId, responseMessage, { parse_mode: "Markdown" })
     }
     else{
-        if(botResponse && botResponse.error){
-            bot.sendMessage(chatId, botResponse.error, { parse_mode: "Markdown" })
+        if(botResponse && botResponse.answer && botResponse.answer.error){
+            bot.sendMessage(chatId, botResponse.answer.error, { parse_mode: "Markdown" })
         }
         else{
             bot.sendMessage(chatId, 'Unknown Error, probably cant connect to bot gateway', { parse_mode: "Markdown" })

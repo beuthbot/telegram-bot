71be9316cc43a357b5caa5c7aaa0ac47b4378642
RobertHalwass <30471969+RobertHalwass@users.noreply.github.com>
2021-02-09 19:20:18 +0100

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -5,10 +5,14 @@ async function configureSocket(gateway, bot) {
         console.log('send message from socket', message);
         await bot.sendMessage(message.userId, message.message);
     })
-
+    
     sock.onFile(async file=>{
         console.log('send file from socket', file.userId, file.fileName);
         const buffer = Buffer.from(file.binary);
+        const fileOptions = {
+          filename: 'audioResponse',
+          contentType: 'audio/mpeg',
+        };
         await bot.sendVoice(file.userId, buffer);
     })
 }
